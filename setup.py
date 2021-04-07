a60fad2744606149ecdccaa7df6941298b7a32dc
Sam Cohan <sam.cohan@udemy.com>
2021-04-07 15:37:32 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup_args = dict(
+    name="scpyutils",
+    version="0.0.1",
+    author="Sam Cohan",
+    author_email="samcohan007@gmail.com",
+    keywords=[
+        "Python Utilities",
+        "Generic Utilities",
+    ],
+    description="Collection of various utilities used by Sam Cohan and may be others?",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sam-cohan/scpyutils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
+install_requires = [
+    "dill",
+    "joblib",
+    "pandas",
+    "scipy",
+]
+
+if __name__ == "__main__":
+    setuptools.setup(**setup_args, install_requires=install_requires)
