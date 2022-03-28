f65a2800f9b012f849f37b7fc11870c402c73f34
Sam Cohan <sam.cohan@udemy.com>
2022-03-28 14:52:21 -0700

    Up minor version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup_args = dict(
     name="scpyutils",
-    version="0.0.1",
+    version="0.0.2",
     author="Sam Cohan",
     author_email="samcohan007@gmail.com",
     keywords=[
