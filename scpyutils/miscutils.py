9f878a0c75562582486551e040c7a2eb9d37b9d8
Sam Cohan <scohan@bigpanda.io>
2024-08-13 17:32:10 -0700

    Add diff_str to miscutils

--- scpyutils/miscutils.py
+++ scpyutils/miscutils.py
@@ -4,16 +4,125 @@ Miscellaneous Utilities.
 Author: Sam Cohan
 """
 
+import difflib
 import importlib
+import re
 import time
 from functools import wraps
 from itertools import zip_longest
-from typing import Any, Callable, Dict, Iterable, Optional, Tuple, Union
+from typing import Any, Callable, Dict, Iterable, Literal, Optional, Tuple, Union
 
 from scpyutils.cacheutils import get_func_name
 from scpyutils.logutils import setup_logger
 
 LOGGER = setup_logger(__name__)
+
+
+def diff_str(  # noqa: C901
+    text1: str,
+    text2: str,
+    diff_level: Literal["char", "word"] = "char",
+    html_output: bool = False,
+    mark_spaces: bool = False,
+) -> str:
+    """
+    Generate a diff between two strings at the specified diff level.
+
+    Args:
+        text1: The first string to compare.
+        text2: The second string to compare.
+        diff_level: The level of comparison ('char' or 'word'). Default is 'char'.
+        html_output: If True, returns the diff in HTML format. Default is False.
+        mark_spaces: Whether to mark spaces with '␣' for emphasis. Default is False.
+
+    Returns:
+        A string representing the differences between text1 and text2.
+    """
+    space_placeholder = "␣" if mark_spaces else " "
+    if diff_level == "word":
+        text1_seq = re.findall(r"\S+|\s+", text1)
+        text2_seq = re.findall(r"\S+|\s+", text2)
+    else:  # 'char' level diff
+        text1_seq = list(text1)
+        text2_seq = list(text2)
+
+    matcher = difflib.SequenceMatcher(None, text1_seq, text2_seq)
+    diff_output = []
+
+    for tag, i1, i2, j1, j2 in matcher.get_opcodes():
+        if tag == "replace":
+            text1_part = "".join(text1_seq[i1:i2]).replace(" ", space_placeholder)
+            text2_part = "".join(text2_seq[j1:j2]).replace(" ", space_placeholder)
+
+            # Adjust leading space
+            if i1 > 0 and text1_seq[i1 - 1].isspace():
+                diff_output.append(" ")
+
+            diff_output.append(
+                (
+                    f"<span style='background-color:#ffdddd;'>[-{text1_part}]</span>"
+                    f"<span style='background-color:#ddffdd;'>[+{text2_part}]</span>"
+                )
+                if html_output
+                else f"[-{text1_part}][+{text2_part}]"
+            )
+
+            # Adjust trailing space
+            if i2 < len(text1_seq) and text1_seq[i2 : i2 + 1] == [" "]:
+                diff_output.append(" ")
+        elif tag == "delete":
+            if i1 > 0 and text1_seq[i1 - 1].isspace():
+                diff_output.append(" ")
+            diff_output.append(
+                f"<span style='background-color:#ffdddd;'>"
+                f"[-{''.join(text1_seq[i1:i2]).replace(' ', space_placeholder)}]</span>"
+                if html_output
+                else f"[-{''.join(text1_seq[i1:i2]).replace(' ', space_placeholder)}]"
+            )
+        elif tag == "insert":
+            if j1 > 0 and text2_seq[j1 - 1].isspace():
+                diff_output.append(" ")
+            diff_output.append(
+                f"<span style='background-color:#ddffdd;'>"
+                f"[+{''.join(text2_seq[j1:j2]).replace(' ', space_placeholder)}]</span>"
+                if html_output
+                else f"[+{''.join(text2_seq[j1:j2]).replace(' ', space_placeholder)}]"
+            )
+        elif tag == "equal":
+            diff_output.append("".join(text1_seq[i1:i2]))
+
+    diff_output_str = "".join(diff_output)
+    if html_output:
+        diff_output_str = diff_output_str.replace("\n", "<br>")
+
+    return diff_output_str
+
+
+def display_diff_str(
+    text1: str,
+    text2: str,
+    diff_level: Literal["char", "word"] = "char",
+    mark_spaces: bool = False,
+) -> None:
+    """
+    Display a diff between two strings using HTML.
+
+    Args:
+        text1: The first string to compare.
+        text2: The second string to compare.
+        diff_level: The level of comparison ('char' or 'word'). Default is 'char'.
+        mark_spaces: Whether to mark spaces with '␣' for emphasis. Default is False.
+    """
+    from IPython.display import HTML, display  # type: ignore
+
+    diff_html = diff_str(
+        text1,
+        text2,
+        diff_level=diff_level,
+        html_output=True,
+        mark_spaces=mark_spaces,
+    )
+    display(HTML(diff_html))
 
 
 def diff_rec(  # noqa: C901
